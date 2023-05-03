046334b4e89b5e8b05ba0e5b41bd2c00585e5f6e
erzaa123 <118440186+erzaa123@users.noreply.github.com>
2023-05-03 14:04:10 +0200

    Add files via upload

---
+++ QuizApp/src/helpers/Questions.js
@@ -0,0 +1,18 @@
+export const Questions = [
+ {
+    prompt: "Which of this is not a programming language?",
+    optionA: "Python",
+    optionB: "JavaScript",
+    optionC: "MC-03",
+    optionD: "Java",
+    asnwer: "optionC",
+  },
+  {
+    prompt: "Which of this is not a javascript framework?",
+    optionA: "React",
+    optionB: "Angular",
+    optionC: "Vue",
+    optionD: "Java",
+    asnwer: "optionD",
+  },
+];
