b643cc8cbd07de3e0d9e8704e5ee7bc4b5e31bbc
erzaa123 <erza.haxhiajdini@gmail.com>
2023-06-05 20:48:36 +0200

    init

---
+++ crud-app-master/src/components/Dashboard/Add.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import Swal from 'sweetalert2';
+
+const Add = ({ users, setUsers, setIsAdding }) => {
+  const [Name, setName] = useState('');
+  const [Points, setPoints] = useState('');
+  
+
+  const handleAdd = e => {
+    e.preventDefault();
+
+    if (!Name || !Points ) {
+      return Swal.fire({
+        icon: 'error',
+        title: 'Error!',
+        text: 'All fields are required.',
+        showConfirmButton: true,
+      });
+    }
+
+    const id = users.length + 1;
+    const newUser = {
+      id,
+      Name,
+      Points,
+    };
+
+    users.push(newUser);
+    localStorage.setItem('usersData', JSON.stringify(users));
+    setUsers(users);
+    setIsAdding(false);
+
+    Swal.fire({
+      icon: 'success',
+      title: 'Added!',
+      text: `${Name} ${Points}'s data has been Added.`,
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  };
+
+  return (
+    <div className="small-container">
+      <form onSubmit={handleAdd}>
+        <h1>Add User</h1>
+        <label htmlFor="Name">First Name</label>
+        <input
+          id="Name"
+          type="text"
+          name="Name"
+          value={Name}
+          onChange={e => setName(e.target.value)}
+        />
+        <label htmlFor="Points">Points</label>
+        <input
+          id="Points"
+          type="text"
+          name="Points"
+          value={Points}
+          onChange={e => setPoints(e.target.value)}
+        />
+       
+        <div style={{ marginTop: '30px' }}>
+          <input type="submit" value="Add" />
+          <input
+            style={{ marginLeft: '12px' }}
+            className="muted-button"
+            type="button"
+            value="Cancel"
+            onClick={() => setIsAdding(false)}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Add;
---
+++ crud-app-master/src/components/Dashboard/Table.js
@@ -0,0 +1,64 @@
+import React from 'react';
+
+const Table = ({ users, handleEdit, handleDelete }) => {
+  users.forEach((user, i) => {
+    user.id = i + 1;
+  });
+
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: null,
+  });
+
+  return (
+    <div className="contain-table">
+      <table className="striped-table">
+        <thead>
+          <tr>
+            <th>No.</th>
+            <th>Name</th>
+            <th>Points</th>
+            
+            <th colSpan={2} className="text-center">
+              Actions
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.length > 0 ? (
+            users.map((user, i) => (
+              <tr key={user.id}>
+                <td>{i + 1}</td>
+                <td>{user.Name}</td>
+                <td>{user.Points}</td>
+                <td className="text-right">
+                  <button
+                    onClick={() => handleEdit(user.id)}
+                    className="button muted-button"
+                  >
+                    Edit
+                  </button>
+                </td>
+                <td className="text-left">
+                  <button
+                    onClick={() => handleDelete(user.id)}
+                    className="button muted-button"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan={7}>No Users</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
