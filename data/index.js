df0e68d5dc63bf3ea9506231102bb99c362b8d33
erzaa123 <erza.haxhiajdini@gmail.com>
2023-06-12 16:14:31 +0200

    init

--- data/index.js
+++ data/index.js
@@ -4,6 +4,26 @@ const usersData = [
     Name: 'Susan',
     Points: 3,
   },
+  {
+    id:6,
+    Name:'Pansa',
+    Points: 4,
+  },
+  {
+    id:3,
+    Name:'gresa',
+    Points: 4,
+  },
+  {
+    id:2,
+    Name:'gustavo',
+    Points: 4,
+  },
+  {
+    id:4,
+    Name:'Juan',
+    Points: 4,
+  }
  
 ];
 
